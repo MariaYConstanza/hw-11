f92bd95e5eecbd76c406b82a7fb6311199d908a1
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-10-31 21:52:17 -0400

    post function in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -6,20 +6,33 @@ const PORT = process.env.port || 3001;
 
 const app = express();
 
-// set up express app and data parsing
+// app express functions for port
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 app.use(express.static('public'));
 
+// set up express app and data parsing
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/api/notes', (req, res) => res.json(noteData));
 
+// terminal link 
 app.listen(PORT, () => {
     console.log(`Your app listening at http://localhost:${PORT} check it out!`);
 });
 
+
+
 // set up POST request and review
+app.post('/api/notes', (req, res) => {
+    res.json(`${req.method} request recieved to add a note`);
+
+    console.info(`${req.method} request recieved to add a note`);
+});
+
+app.listen(PORT, () => 
+console.log(`Express server listening on ${PORT}`)
+);
