a8c94f20656399dd6741d385c6595b83050ed034
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-10-31 21:05:18 -0400

    express and get functions hard code in server.js file

---
+++ Develop/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = ('path');
+const noteData = require ('./db/db.json');
+
+const PORT = 3001;
+
+const app = express();
+
+// set up express app and data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/notes', (req, res) => res.json(noteData));
+
+app.listen(PORT, () => {
+    console.log('Your app listening at http://localhost:${PORT} check it out!');
+});
