d22f22ddd29f496601e26d5faafbec1814c43259
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-11-01 18:06:51 -0400

    comment in helpers folder uuid.js file

--- Develop/helpers/uuid.js
+++ Develop/helpers/uuid.js
@@ -1,4 +1,5 @@
-// Immediately export a function that generates a string of random numbers and letters
+// Immediately export a function that generates a randomized string that creates
+// characters such as letters and numbers
 
 module.exports = () =>
   Math.floor((1 + Math.random()) * 0x10000)
