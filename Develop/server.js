7228330c243c9368b0b894e43a3fc6a927c4f3e9
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-11-01 20:24:15 -0400

    created notes function in notes.js, index.js and server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = ('path');
 const noteData = require ('./db/db.json');
+const api = require('./routes/index.js');
 
 const PORT = process.env.port || 3001;
 
 const app = express();
+
 
 // app express functions for port
 app.use(express.urlencoded({ extended: true}));
@@ -14,7 +16,7 @@ app.use(express.static('public'));
 
 // set up express app and data parsing
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/api/notes', (req, res) => res.json(noteData));
@@ -23,20 +25,3 @@ app.get('/api/notes', (req, res) => res.json(noteData));
 app.listen(PORT, () => {
     console.log(`Your app listening at http://localhost:${PORT} check it out!`);
 });
-
-app.get('/api/notes', (req, res) => {
-    res.json(`${req.method} request received to get notes`);
-  
-    console.info(`${req.method} request received to get notes`);
-  });
-
-// set up POST request and review
-app.post('/api/notes', (req, res) => {
-    res.json(`${req.method} request recieved to add a note`);
-
-    console.info(`${req.method} request recieved to add a note`);
-});
-
-app.listen(PORT, () => 
-console.log(`Express server listening on ${PORT}`)
-);
