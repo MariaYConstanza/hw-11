a2673e2b547b10a9645effb26b520bba848fe860
MariaYConstanza <mariaysabelconstanza@gmail.com>
2022-11-01 21:55:07 -0400

    server.js 500 error still continues for note.html connection

--- Develop/server.js
+++ Develop/server.js
@@ -19,7 +19,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.get('/api/notes', (req, res) => res.json(noteData));
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
 // terminal link 
 app.listen(PORT, () => {
